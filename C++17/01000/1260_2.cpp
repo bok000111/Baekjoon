1c81e565dc4e9bbe9f1454d396c3fb6d405b7a26
bok000111 <bok000111@naver.com>
2022-05-31 01:29:48 +0900

    solution update

--- C++17/01000/1260_2.cpp
+++ C++17/01000/1260_2.cpp
@@ -7,7 +7,7 @@
 int main() {
 
     std::vector<short> graph[1001];
-    bool visit[1001];
+    bool visit1[1001], visit2[1001];
     short n, m, r, tmp;
 
     scanf("%hd %hd %hd", &n, &m, &r);
@@ -31,37 +31,34 @@ int main() {
     while(!dfs.empty()) {
         v = dfs.top();
         dfs.pop();
-        if(!visit[v]) {
-            visit[v] = 1;
+        if(!visit1[v]) {
+            visit1[v] = 1;
             printf("%hd ", v);
             tmp = graph[v].size();
             for(short i = tmp-1; i >= 0; i--) {
-                if(!visit[graph[v][i]]) {
+                if(!visit1[graph[v][i]]) {
                     dfs.push(graph[v][i]);
                 }
             }
         }
     }
 
-    for(short i = 1; i <= n; i++) {
-        visit[i] = 0;
-    }
     printf("\n");
 
     depth = 1, v = r;
     std::queue<short> bfs;
     bfs.push(v);
-    visit[0] = true;
+    visit2[0] = true;
 
     while(!bfs.empty()) {
         v = bfs.front();
         bfs.pop();
-        if(!visit[v]) {
-            visit[v] = 1;
+        if(!visit2[v]) {
+            visit2[v] = 1;
             printf("%hd ", v);
             tmp = graph[v].size();
             for(int i = 0; i < tmp; i++) {
-                if(!visit[graph[v][i]]) {
+                if(!visit2[graph[v][i]]) {
                     bfs.push(graph[v][i]);
                 }
             }
